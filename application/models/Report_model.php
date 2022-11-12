f022d63b3e4a5cccb87565543e6144cb938b7834
jazy29 <jonathanbuccat29@gmail.com>
2022-11-12 18:48:19 +0800

    timenow

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -34,7 +34,7 @@ class Report_model extends CI_Model {
         $this->title            = $post['title'];
         $this->description      = $post['description'];
         $this->type             = $post['type'];
-        $this->date_reported    = mysql_to_unix('date');
+        $this->date_reported    = date(now());
         $this->file             = $this->_uploadFile();
 
         return $this->db->insert('user_report', $this);
